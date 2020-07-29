cbdfc83546dfed5647f20983ae80a8b19700f0d1
Trevor Alexander Jefferson <coinoperatedguy4@gmail.com>
2020-07-28 23:28:00 -0400

    TrevorJ-2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = props => {
   return (
     <>
-      <figure className="image is-128x128">
+      <h1>WELCOME TO CAR SALES!!</h1>
+      <figure className="image">
         <img src={props.car.image} alt={props.car.name} />
       </figure>
       <h2>{props.car.name}</h2>
